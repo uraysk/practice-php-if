77c92cd7e9b0f82c0305d321e295392116127d4d
Ura20 <ura2091.212@gmail.com>
2024-11-09 15:57:00 +0900

    feat:計算結果表示２

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,35 @@
 <?php
-echo 'Welcome TECH QUEST!';
+echo " <b>4-7</b><br>";
+//num1が8を出力するようにデバッグ
+$food = 7;
+$num1 = 0;
+if($food % 2 == 0){
+    $num1 = $food / 2;
+}
+else{
+    $num1 = ($food - 1) / 2;
+    //was if($num > 3)
+    if($num1 > 2){
+        $num1 += 5;
+    }
+    else{
+        $num1 += 6;
+    }
+}
+echo "課題１ : " . $num1 . "<br/>";
+
+//num2がnum2は１２の倍数かつ２４の倍数です。を出力するようにデバッグ
+//ifで複数条件の指定、$num2 % 12 == 0はelseifへ
+$num2 = 72;
+if ($num2 % 12 == 0 && $num2 % 24 == 0) {
+  echo "課題２：numの値は12の倍数かつ、24の倍数です<br/>";
+}
+elseif ($num2 % 12 == 0) {
+    echo "課題２：numの値は12の倍数です";
+} 
+elseif ($num2 % 24 == 0) {
+    echo "課題２：numの値は24の倍数です<br/>";
+}
+
+
+
