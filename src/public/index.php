7b715a22cb2d56a2257ee4106fe0923727185cdf
ura <45548267+uraysk@users.noreply.github.com>
2024-11-09 19:12:13 +0900

    refactor:index.phpを修正
    gitの仕組みについて勘違いしていたので、別ブランチで作成した課題１の機能の部分を削除しました。

--- src/public/index.php
+++ src/public/index.php
@@ -1,23 +1,4 @@
 <?php
-echo " <b>4-7</b><br>";
-//num1が8を出力するようにデバッグ
-$food = 7;
-$num1 = 0;
-if($food % 2 == 0){
-    $num1 = $food / 2;
-}
-else{
-    $num1 = ($food - 1) / 2;
-    //was if($num > 3)
-    if($num1 > 2){
-        $num1 += 5;
-    }
-    else{
-        $num1 += 6;
-    }
-}
-echo "課題１ : " . $num1 . "<br/>";
-
 //num2がnum2は１２の倍数かつ２４の倍数です。を出力するようにデバッグ
 //ifで複数条件の指定、$num2 % 12 == 0はelseifへ
 $num2 = 72;
