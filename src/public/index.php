2fc901fc68923dfb3a4efe01413e7a706f47de23
Ura20 <ura2091.212@gmail.com>
2024-11-11 20:03:50 +0900

    feat:ビルトイン関数１

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$arrayList = [
+	[3, 5, 1, 0, 9, 12],
+	[1, 2, 14, 10, 19, 1],
+	[0, 3, 3, 3, 7, 2]
+];
+foreach ($arrayList as $array) {
+    echo max($array)."<br/>";
+}
