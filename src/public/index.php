f40a5775048d39c60733710277d8d7ea8e2dd39d
ura <45548267+uraysk@users.noreply.github.com>
2024-11-16 14:28:55 +0900

    fix:表示方法の修正
    タイトルと課題番号を追加しました。

--- src/public/index.php
+++ src/public/index.php
@@ -1,24 +1,26 @@
 <?php
 $word = "fuga";
 
+echo "<b>4-10</b><br/>";
+echo "課題１：";
 for ($i = 1; $i <= 6; $i++) {
     if ($word == "hoge" && $i % 2 == 0) {
-        echo "wordがhoge かつ " . $i . "は2の倍数です<br/>";
+        echo "wordがhoge かつ " . $i . "は2の倍数です<br/>　　　　";
         continue;
     }
 
     if($word == "hoge" && $i % 2 == 1){
-        echo "wordがhoge かつ " . $i . "は2の倍数以外です<br/>";
+        echo "wordがhoge かつ " . $i . "は2の倍数以外です<br/>　　　　";
         continue;
     }
     
     if ($i % 2 == 0) {
-        echo "wordがhogeでない かつ " . $i . "は2の倍数です<br/>";
+        echo "wordがhogeでない かつ " . $i . "は2の倍数です<br/>　　　　";
         continue;
     }
 
     if($i % 2 == 1){
-        echo "wordがhogeでない かつ " . $i . "は2の倍数以外です<br/>";
+        echo "wordがhogeでない かつ " . $i . "は2の倍数以外です<br/>　　　　";
         continue;
     }
 }
